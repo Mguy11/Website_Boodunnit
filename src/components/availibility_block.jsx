6879824d4bb9fc8e5f764cbab11f62e3734fd328
Martijn <0911554@hr.nl>
2021-01-18 11:11:53 +0100

    removed itch link

--- src/components/availibility_block.jsx
+++ src/components/availibility_block.jsx
@@ -13,7 +13,6 @@ const AvailibilityBlock = (props) => {
             <h1 className="availibility__title">Available Now</h1>
             <div className="devider" />
             <div className="availibility__gif" style={{ backgroundImage: `url(${availableGif})` }}></div>
-            <iframe frameborder="0" src="https://itch.io/embed/857493?border_width=5&amp;bg_color=a792ac&amp;fg_color=36100a&amp;link_color=e67245&amp;border_color=a792ac" width="560" height="175"><a href="https://studiokoprol.itch.io/boodunnit%22%3EBoodunnit?!"> by Studio Koprol</a></iframe>
             {/* <div className="availibility__box" /> */}
             <div className="availibility__icons">
               {/* <PlatformButton
@@ -37,7 +36,6 @@ const AvailibilityBlock = (props) => {
                 altText="gog-icon"
               /> */}
             </div>
-            <span className="availibility__soon">*other platforms coming soon...</span>
           </div>
         </Col>
       </Row>
